b71b7a85e7fa491fba12d0256b49f8bde1e8a245
fantadn <62031687+fantadn@users.noreply.github.com>
2021-01-25 17:05:32 -0500

    project created with react app and files added for home page

---
+++ src/homepage.component.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+import './homepage.styles.scss'
+
+const HomePage = () => (
+    <div className='homepage'>
+      <div className='directory-menu'>
+        <div className ='menu-item'>
+          <div className='content'>
+            <h1 className='tile'>HATS</h1>
+             <span className ='subtitle'>SHOP NOW</span>
+          </div>
+        </div>
+
+        <div className ='menu-item'>
+          <div className='content'>
+            <h1 className='tile'>JACKETS</h1>
+             <span className ='subtitle'>SHOP NOW</span>
+          </div>
+        </div>
+
+        <div className ='menu-item'>
+          <div className='content'>
+            <h1 className='tile'>SNEAKERS</h1>
+             <span className ='subtitle'>SHOP NOW</span>
+          </div>
+        </div>
+        
+        <div className ='menu-item'>
+          <div className='content'>
+            <h1 className='tile'>WOMEN</h1>
+             <span className ='subtitle'>SHOP NOW</span>
+          </div>
+        </div>
+
+        <div className ='menu-item'>
+          <div className='content'>
+            <h1 className='tile'>MEN</h1>
+             <span className ='subtitle'>SHOP NOW</span>
+          </div>
+        </div>
+
+
+      </div>
+    </div>
+);
+
+
+export default HomePage;
+
